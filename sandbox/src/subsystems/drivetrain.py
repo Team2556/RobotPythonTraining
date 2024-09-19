d885792e13430e25a3a1a724844f96363f704b4a
wmbelk <61924388+wmbelk@users.noreply.github.com>
2024-09-18 22:33:09 -0500

    add path planner and Xdrive

--- sandbox/src/subsystems/drivetrain.py
+++ sandbox/src/subsystems/drivetrain.py
@@ -12,6 +12,7 @@ import phoenix5
 from constants import DriveConstant
 
 import typing
+import math
 
 class DriveTrain(commands2.Subsystem):
     def __init__(self) -> None:
@@ -50,15 +51,26 @@ class DriveTrain(commands2.Subsystem):
         self.backLeftMotor.getSensorCollection().setQuadraturePosition(0, 10)
         self.backRightMotor.getSensorCollection().setQuadraturePosition(0, 10)
         '''
-        if DriveConstant.kIsMecanum:
-            self.robotDrive = wpilib.drive.MecanumDrive(frontLeftMotor= self.frontLeftMotor, 
-                                                        frontRightMotor=self.frontRightMotor, 
-                                                        rearLeftMotor=self.backLeftMotor, 
-                                                        rearRightMotor=self.backRightMotor)
-        else:
-            self.backLeftMotor.follow(self.frontLeftMotor)
-            self.backRightMotor.follow(self.frontRightMotor)
-            self.robotDrive = wpilib.drive.DifferentialDrive(self.frontLeftMotor, self.frontRightMotor)
+        match DriveConstant.kDriveTye:
+            case "Xdrive":
+                self.robotDrive = TheWB_Xdrive(self.frontLeftMotor, self.frontRightMotor, self.backLeftMotor, self.backRightMotor)
+            case "Tank":
+                self.robotDrive = wpilib.drive.DifferentialDrive(self.frontLeftMotor, self.frontRightMotor)
+            case "Mecanum": 
+                self.robotDrive = wpilib.drive.MecanumDrive(frontLeftMotor= self.frontLeftMotor,    
+                                                            frontRightMotor=self.frontRightMotor,
+                                                            rearLeftMotor=self.backLeftMotor,
+                                                            rearRightMotor=self.backRightMotor)
+        
+        # if DriveConstant.kIsMecanum:
+        #     self.robotDrive = wpilib.drive.MecanumDrive(frontLeftMotor= self.frontLeftMotor, 
+        #                                                 frontRightMotor=self.frontRightMotor, 
+        #                                                 rearLeftMotor=self.backLeftMotor, 
+        #                                                 rearRightMotor=self.backRightMotor)
+        # else:
+        #     self.backLeftMotor.follow(self.frontLeftMotor)
+        #     self.backRightMotor.follow(self.frontRightMotor)
+        #     self.robotDrive = wpilib.drive.DifferentialDrive(self.frontLeftMotor, self.frontRightMotor)
         
         
         self.robotDrive.setMaxOutput(0.60)
@@ -84,6 +96,8 @@ class DriveTrain(commands2.Subsystem):
             self.robotDrive.driveCartesian(joystick.getLeftX(), joystick.getRightX(),-joystick.getLeftY(),  Rotation2d(0))
         elif isinstance(self.robotDrive, wpilib.drive.DifferentialDrive):
             self.robotDrive.arcadeDrive(joystick.getLeftY(), -joystick.getLeftX())
+        else:
+            self.robotDrive.TheWB_driveCartesian(-joystick.getLeftY(), -joystick.getLeftX(), -joystick.getRightX())
     def slowLeft(self,joystick: wpilib.Joystick) -> None:
         self.robotDrive.driveCartesian(0, 0, -.22, Rotation2d(0))
     def slowRight(self,joystick: wpilib.Joystick) -> None:
@@ -97,3 +111,63 @@ class DriveTrain(commands2.Subsystem):
         self.backLeftMotor.set(0.53)
     def OnlyBackRight(self) -> None:
         self.backRightMotor.set(0.54)
+
+class TheWB_Xdrive:
+    def __init__(self, frontLeftmotor, frontRightmotor, backLeftmotor, backRightmotor):
+        self.frontLeftmotor = frontLeftmotor
+        self.backLeftmotor = backLeftmotor
+        self.frontRightmotor = frontRightmotor
+        self.backRightmotor = backRightmotor
+        
+        self.Deadband = 0.1
+        self.MaxOutput = .995
+    
+    def setMaxOutput(self, maxOutput: float):
+        self.MaxOutput = maxOutput
+
+    def TheWB_driveCartesian(self, xSpeed, ySpeed, zRotation): #, gyroAngle = 0.0):
+        '''
+        xSpeed: The speed that the robot should drive in its X direction. [-1.0..1.0]
+        ySpeed: The speed that the robot should drive in its Y direction. [-1.0..1.0]
+        zRotation: The rate of rotation for the robot that is independent of the translation. [-1.0..1.0]
+        '''
+        for controlInput in [xSpeed, ySpeed, zRotation]:
+            if abs(controlInput) < self.Deadband:
+                controlInput = 0.0
+            else:
+                controlInput = controlInput# TODO: consider smoothing or squaring (abs(controlInput) - self.Deadband) * controlInput / abs(controlInput)
+        
+        #create coding for the mecanum drive kinematics
+        base_theta = math.atan2(xSpeed, ySpeed) - math.pi / 4.0
+        r = math.hypot(xSpeed, ySpeed)
+        cos = math.cos(base_theta)
+        sin = math.sin(base_theta)
+        max_trig = max(abs(cos), abs(sin))
+        leftFront = r * cos/max_trig + zRotation
+        rightFront = r * sin/max_trig - zRotation
+        leftRear = r * sin/max_trig + zRotation
+        rightRear = r * cos/max_trig - zRotation
+
+        # Limit the toal power to the motors to self.MaxOutput
+
+        maxMagnitude = max(max(abs(leftFront), abs(rightFront)), max(abs(leftRear), abs(rightRear)))
+        if maxMagnitude > self.MaxOutput:
+            leftFront /= maxMagnitude/self.MaxOutput
+            rightFront /= maxMagnitude/self.MaxOutput
+            leftRear /= maxMagnitude/self.MaxOutput
+            rightRear /= maxMagnitude/self.MaxOutput
+        SmartDashboard.putNumber("leftFront in TheWB_drivecartesian", leftFront)
+        SmartDashboard.putNumber("rightFront in TheWB_drivecartesian", rightFront)
+        SmartDashboard.putNumber("leftRear in TheWB_drivecartesian", leftRear)
+        SmartDashboard.putNumber("rightRear in TheWB_drivecartesian", rightRear)
+
+        # TODO: should this just set motor speeds instead of return something
+        self.frontLeftmotor.set( leftFront)
+        # self.frontLeftmotor_control.with_output(leftFront))
+        self.frontRightmotor.set(rightFront)
+        self.backLeftmotor.set(leftRear)  
+        self.backRightmotor.set( rightRear)  
+
+
+        return (leftFront, rightFront, leftRear, rightRear)
+
